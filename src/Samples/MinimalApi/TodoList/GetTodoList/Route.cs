2f1b11e44168c7e53c478067c3ea1f0ff35176bd
Matt Burton <matt.burton@gmail.com>
2024-04-30 08:32:51 -0400

    update sample

--- src/Samples/MinimalApi/TodoList/GetTodoList/Route.cs
+++ src/Samples/MinimalApi/TodoList/GetTodoList/Route.cs
@@ -8,8 +8,8 @@ public static class Route
         {
             var stream = await eventStore.ReadStream(StreamName.For<TodoList>(id), cancellationToken);
 
-            return stream.IsEmpty ? Results.NotFound() : Results.Ok(stream.ProjectTo<TodoList>());
-        }).Produces<TodoList>().WithName("Get Todo List").WithOpenApi();
+            return stream.IsEmpty ? Results.NotFound() : Results.Ok(stream.ProjectTo<TodoListView>());
+        }).Produces<TodoListView>().WithName("Get Todo List").WithOpenApi();
 
         return builder;
     }
