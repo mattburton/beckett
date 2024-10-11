34ce60b21f2c48f9b24afa69a17c145048032f12
Matt Burton <matt.burton@gmail.com>
2024-10-11 13:07:13 -0400

    add Polly support

--- src/Beckett/MessageStorage/Postgres/PostgresMessageStorage.cs
+++ src/Beckett/MessageStorage/Postgres/PostgresMessageStorage.cs
@@ -1,6 +1,9 @@
 using Beckett.Database;
 using Beckett.Database.Types;
 using Beckett.MessageStorage.Postgres.Queries;
+using Npgsql;
+using Polly;
+using Polly.Retry;
 
 namespace Beckett.MessageStorage.Postgres;
 
@@ -15,8 +18,11 @@ public class PostgresMessageStorage(IPostgresDatabase database, PostgresOptions 
     {
         var newMessages = messages.Select(x => MessageType.From(streamName, x)).ToArray();
 
-        var streamVersion = await database.Execute(
-            new AppendToStream(streamName, expectedVersion.Value, newMessages, options),
+        var query = new AppendToStream(streamName, expectedVersion.Value, newMessages, options);
+
+        var streamVersion = await Pipeline.ExecuteAsync(
+            static async (state, token) => await state.database.Execute(state.query, token),
+            (database, query),
             cancellationToken
         );
 
@@ -29,8 +35,11 @@ public class PostgresMessageStorage(IPostgresDatabase database, PostgresOptions 
         CancellationToken cancellationToken
     )
     {
-        var results = await database.Execute(
-            new ReadGlobalStream(lastGlobalPosition, batchSize, options),
+        var query = new ReadGlobalStream(lastGlobalPosition, batchSize, options);
+
+        var results = await Pipeline.ExecuteAsync(
+            static async (state, token) => await state.database.Execute(state.query, token),
+            (database, query),
             cancellationToken
         );
 
@@ -61,8 +70,11 @@ public class PostgresMessageStorage(IPostgresDatabase database, PostgresOptions 
         CancellationToken cancellationToken
     )
     {
-        var streamMessages = await database.Execute(
-            new ReadStream(streamName, readOptions, options),
+        var query = new ReadStream(streamName, readOptions, options);
+
+        var streamMessages = await Pipeline.ExecuteAsync(
+            static async (state, token) => await state.database.Execute(state.query, token),
+            (database, query),
             cancellationToken
         );
 
@@ -88,4 +100,15 @@ public class PostgresMessageStorage(IPostgresDatabase database, PostgresOptions 
 
         return new ReadStreamResult(streamName, streamVersion, messages);
     }
+
+    private static readonly ResiliencePipeline Pipeline = new ResiliencePipelineBuilder().AddRetry(
+        new RetryStrategyOptions
+        {
+            ShouldHandle = new PredicateBuilder().Handle<NpgsqlException>(),
+            MaxRetryAttempts = 3,
+            Delay = TimeSpan.FromMilliseconds(50),
+            BackoffType = DelayBackoffType.Exponential,
+            UseJitter = true
+        }
+    ).Build();
 }
