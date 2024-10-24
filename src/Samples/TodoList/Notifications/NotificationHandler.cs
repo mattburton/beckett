8703549b8ecbad073b9fe84077dd08945db6bb1e
Matt Burton <matt.burton@gmail.com>
2024-10-24 12:19:29 -0400

    update sample handlers to use ILogger<T>

--- src/Samples/TodoList/Notifications/NotificationHandler.cs
+++ src/Samples/TodoList/Notifications/NotificationHandler.cs
@@ -1,17 +1,18 @@
+using Microsoft.Extensions.Logging;
 using TodoList.AddItem;
 using TodoList.CompleteItem;
 using TodoList.CreateList;
 
 namespace TodoList.Notifications;
 
-public class NotificationHandler
+public class NotificationHandler(ILogger<NotificationHandler> logger)
 {
     public Task Handle(IMessageContext context, CancellationToken _)
     {
         switch (context.Message)
         {
             case TodoListCreated e:
-                Console.WriteLine($"List created [Id: {e.TodoListId}]");
+                logger.LogInformation("List created [Id: {TodoListId}]", e.TodoListId);
                 break;
             case TodoListItemAdded e:
                 if (e.Item.Contains("Retry"))
@@ -24,10 +25,10 @@ public class NotificationHandler
                     throw new TerminalException(e.Item);
                 }
 
-                Console.WriteLine($"Item added: {e.Item} [List: {e.TodoListId}]");
+                logger.LogInformation("Item added: {Item} [List: {TodoListId}]", e.Item, e.TodoListId);
                 break;
             case TodoListItemCompleted e:
-                Console.WriteLine($"Item completed: {e.Item} [List: {e.TodoListId}]");
+                logger.LogInformation("Item completed: {Item} [List: {TodoListId}]", e.Item, e.TodoListId);
                 break;
         }
 
