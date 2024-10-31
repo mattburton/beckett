747cd81ec03fcb59b832bd692b5ca9da2b5b195f
Matt Burton <matt.burton@gmail.com>
2024-10-30 22:12:24 -0400

    auto-refresh on paged views should maintain current state - page, query, etc...

--- src/Beckett/Dashboard/IPagedViewModel.cs
+++ src/Beckett/Dashboard/IPagedViewModel.cs
@@ -10,9 +10,13 @@ public interface IPagedViewModel
     int From => TotalResults == 0 ? 0 : Page == 1 ? 1 : (Page - 1) * PageSize + 1;
     int To => From + PageSize > TotalResults ? TotalResults : From - 1 + PageSize;
     int TotalPages => (int)Math.Ceiling((double)TotalResults / PageSize);
+}
 
-    string PreviousLink(string urlTemplate) => Page > 1 ? string.Format(urlTemplate, Page - 1, PageSize, Query) : "#";
+public static class PagedViewModelExtensions
+{
+    public static string CurrentLink(this IPagedViewModel model, string urlTemplate) => string.Format(urlTemplate, model.Page, model.PageSize, model.Query);
 
-    string NextLink(string urlTemplate) =>
-        Page < TotalPages ? string.Format(urlTemplate, Page + 1, PageSize, Query) : "#";
+    public static string PreviousLink(this IPagedViewModel model, string urlTemplate) => model.Page > 1 ? string.Format(urlTemplate, model.Page - 1, model.PageSize, model.Query) : "#";
+
+    public static string NextLink(this IPagedViewModel model, string urlTemplate) => model.Page < model.TotalPages ? string.Format(urlTemplate, model.Page + 1, model.PageSize, model.Query) : "#";
 }
