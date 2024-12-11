53a052119882261a55d0d574ae4a34d51452ca26
Matt Burton <matt.burton@gmail.com>
2024-12-10 23:01:06 -0500

    update ProjectTo to handle seed state value

--- src/Beckett/StateExtensions.cs
+++ src/Beckett/StateExtensions.cs
@@ -2,21 +2,10 @@ namespace Beckett;
 
 public static class StateExtensions
 {
-    public static TState ProjectTo<TState>(this IEnumerable<object> messages) where TState : IApply, new() =>
-        messages.Aggregate(
-            new TState(),
-            (current, message) =>
-            {
-                current.Apply(message);
-
-                return current;
-            }
-        );
-
-    public static TState ProjectTo<TState>(this TState seed, IEnumerable<object> messages)
+    public static TState ProjectTo<TState>(this IEnumerable<object> messages, TState? state = default)
         where TState : IApply, new() =>
         messages.Aggregate(
-            seed,
+            state ?? new TState(),
             (current, message) =>
             {
                 current.Apply(message);
