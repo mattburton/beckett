063a30ad4cc6b4aada21b2938bbb37c124dfc7db
Matt Burton <matt.burton@gmail.com>
2024-12-29 18:32:14 -0500

    drop projection filter for now

--- src/Beckett/Projections/IProjectionMessageConfiguration.cs
+++ src/Beckett/Projections/IProjectionMessageConfiguration.cs
@@ -2,20 +2,16 @@ namespace Beckett.Projections;
 
 public interface IProjectionMessageConfiguration<TKey>
 {
-    IProjectionMessageConfiguration<TKey> WithFilter<TMessage>(Predicate<TMessage> filter);
     IProjectionMessageConfiguration<TKey> IgnoreWhenNotFound();
 }
 
 public class ProjectionMessageConfiguration<TKey>(
-    Type messageType,
     ProjectionAction action,
     Func<object, TKey> key
 ) : IProjectionMessageConfiguration<TKey>
 {
-    internal Type MessageType { get; } = messageType;
     internal ProjectionAction Action { get; } = action;
     internal Func<object, TKey> Key { get; } = key;
-    internal Predicate<object>? Filter { get; private set; }
     internal bool IgnoreIfNotFound { get; private set; }
 
     internal static ProjectionMessageConfiguration<TKey> Create<TMessage>(
@@ -23,14 +19,7 @@ public class ProjectionMessageConfiguration<TKey>(
         Func<TMessage, TKey> keySelector
     )
     {
-        return new ProjectionMessageConfiguration<TKey>(typeof(TMessage), action, x => keySelector((TMessage)x));
-    }
-
-    public IProjectionMessageConfiguration<TKey> WithFilter<TMessage>(Predicate<TMessage> filter)
-    {
-        Filter = x => filter((TMessage)x);
-
-        return this;
+        return new ProjectionMessageConfiguration<TKey>(action, x => keySelector((TMessage)x));
     }
 
     public IProjectionMessageConfiguration<TKey> IgnoreWhenNotFound()
