da31e7b89c6a1b07e6210ab58feba66a701d5d5e
Matt Burton <matt.burton@gmail.com>
2025-01-14 11:58:52 -0500

    update state generator to handle Apply methods that accept IMessageContext as the second argument

---
+++ src/Beckett.Tests/SourceGenerators/StateGeneratorTests.cs
@@ -0,0 +1,120 @@
+using System.Reflection;
+using Beckett.Tests.Fakes;
+
+namespace Beckett.Tests.SourceGenerators;
+
+public class StateGeneratorTests
+{
+    public class when_class_is_not_nested
+    {
+        [Fact]
+        public void apply_method_is_generated()
+        {
+            var method = typeof(TestState).GetMethod(
+                nameof(TestState.Apply),
+                BindingFlags.Public | BindingFlags.Instance,
+                [typeof(IMessageContext)]
+            );
+
+            Assert.NotNull(method);
+        }
+
+        [Fact]
+        public void applies_message()
+        {
+            var state = new TestState();
+            const int expectedValue = 1;
+            var message = new TestMessage(expectedValue);
+            var context = new FakeMessageContext(message);
+
+            state.Apply(context);
+
+            Assert.Equal(expectedValue, state.Value);
+        }
+
+        [Fact]
+        public void applies_message_with_context()
+        {
+            var state = new TestState();
+            const int expectedValue = 1;
+            var message = new TestMessageWithContext(expectedValue);
+            var context = new FakeMessageContext(message);
+
+            state.Apply(context);
+
+            Assert.Equal(expectedValue, state.Value);
+            Assert.True(state.ContextSupplied);
+        }
+    }
+
+    public class when_class_is_nested
+    {
+        [Fact]
+        public void apply_method_is_generated_for_nested_class()
+        {
+            var method = typeof(TestContainingClass.NestedState).GetMethod(
+                nameof(TestState.Apply),
+                BindingFlags.Public | BindingFlags.Instance,
+                [typeof(IMessageContext)]
+            );
+
+            Assert.NotNull(method);
+        }
+    }
+
+    public class when_class_is_nested_inside_record
+    {
+        [Fact]
+        public void apply_method_is_generated_for_nested_class()
+        {
+            var method = typeof(TestContainingRecord.NestedState).GetMethod(
+                nameof(TestState.Apply),
+                BindingFlags.Public | BindingFlags.Instance,
+                [typeof(IMessageContext)]
+            );
+
+            Assert.NotNull(method);
+        }
+    }
+}
+
+[State]
+public partial class TestState
+{
+    public int Value { get; private set; }
+    public bool ContextSupplied { get; private set; }
+
+    private void Apply(TestMessage m) => Value = m.Value;
+
+    private void Apply(TestMessageWithContext m, IMessageContext _)
+    {
+        Value = m.Value;
+        ContextSupplied = true;
+    }
+}
+
+public partial class TestContainingClass
+{
+    [State]
+    public partial class NestedState
+    {
+        private void Apply(TestMessage _)
+        {
+        }
+    }
+}
+
+public partial record TestContainingRecord
+{
+    [State]
+    public partial class NestedState
+    {
+        private void Apply(TestMessage _)
+        {
+        }
+    }
+}
+
+public record TestMessage(int Value);
+
+public record TestMessageWithContext(int Value);
