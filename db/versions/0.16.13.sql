257e7df5c3e6e479d884f464246f65ecf499fe92
Matt Burton <matt.burton@gmail.com>
2025-01-31 16:27:41 -0500

    checkpoint preprocessor trigger - do not notify when checkpoints are reserved

---
+++ db/versions/0.16.13.sql
@@ -0,0 +1,22 @@
+-- update checkpoint preprocessor - do not trigger notifications when checkpoints are reserved
+CREATE OR REPLACE FUNCTION beckett.checkpoint_preprocessor()
+  RETURNS trigger
+  LANGUAGE plpgsql
+AS
+$$
+BEGIN
+  IF (TG_OP = 'UPDATE') THEN
+    NEW.updated_at = now();
+  END IF;
+
+  IF (NEW.status = 'active' AND NEW.process_at IS NULL AND NEW.stream_version > NEW.stream_position) THEN
+    NEW.process_at = now();
+  END IF;
+
+  IF (NEW.name != '$global' AND NEW.process_at IS NOT NULL AND NEW.reserved_until IS NULL) THEN
+    PERFORM pg_notify('beckett:checkpoints', NEW.group_name);
+  END IF;
+
+  RETURN NEW;
+END;
+$$;
