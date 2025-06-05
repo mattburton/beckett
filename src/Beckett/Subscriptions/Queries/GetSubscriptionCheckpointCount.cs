6db65fc163480e03efbd678e0c72a7b23160ca3e
Matt Burton <matt.burton@gmail.com>
2025-06-05 18:47:11 -0400

    fix to handle new subscriptions that don't have any work to do after initialization - instead of being stuck in replay they should be set to active

---
+++ src/Beckett/Subscriptions/Queries/GetSubscriptionCheckpointCount.cs
@@ -0,0 +1,41 @@
+using Beckett.Database;
+using Npgsql;
+using NpgsqlTypes;
+
+namespace Beckett.Subscriptions.Queries;
+
+public class GetSubscriptionCheckpointCount(
+    string groupName,
+    string name,
+    PostgresOptions options
+) : IPostgresDatabaseQuery<long>
+{
+    public async Task<long> Execute(NpgsqlCommand command, CancellationToken cancellationToken)
+    {
+        command.CommandText = $@"
+            SELECT count(*)
+            FROM {options.Schema}.checkpoints
+            WHERE group_name = $1
+            AND name = $2;
+        ";
+
+        command.Parameters.Add(new NpgsqlParameter { NpgsqlDbType = NpgsqlDbType.Text });
+        command.Parameters.Add(new NpgsqlParameter { NpgsqlDbType = NpgsqlDbType.Text });
+
+        if (options.PrepareStatements)
+        {
+            await command.PrepareAsync(cancellationToken);
+        }
+
+        command.Parameters[0].Value = groupName;
+        command.Parameters[0].Value = name;
+
+        var result = await command.ExecuteScalarAsync(cancellationToken);
+
+        return result switch
+        {
+            long count => count,
+            _ => throw new Exception($"Unexpected result from subscription checkpoint count: {result}")
+        };
+    }
+}
