834ea1ac94148e827038bb1f890eb5062b28e845
Matt Burton <matt.burton@gmail.com>
2025-08-06 21:36:50 -0400

    version migration cleanup

--- db/versions/0.20.12.sql
+++ db/versions/0.20.12.sql
@@ -1,8 +1,9 @@
--- Beckett v0.20.12 - inline append query
-
+-- Beckett v0.20.12
+ 
+-- inline append query
 DROP FUNCTION IF EXISTS beckett.append_to_stream(text, bigint, beckett.message[]);
 
--- utility function to enable raising exceptions in an inline query
+-- utility function to enable raising exceptions from inline queries
 CREATE OR REPLACE FUNCTION beckett.assert_condition(
   _condition boolean,
   _message text
@@ -19,3 +20,10 @@ BEGIN
   RETURN TRUE;
 END;
 $$;
+
+-- correlated message query performance fix
+DROP INDEX IF EXISTS beckett.ix_messages_active_correlation_id;
+
+CREATE INDEX ix_messages_active_correlation_id_global_position
+  ON beckett.messages_active ((metadata ->> '$correlation_id'::text), global_position)
+  WHERE ((metadata ->> '$correlation_id'::text) IS NOT NULL);
--- db/versions/0.20.13.sql
+++
@@ -1,7 +0,0 @@
--- Beckett v0.20.13 - fix correlation ID query performance issue
-
-DROP INDEX IF EXISTS beckett.ix_messages_active_correlation_id;
-
-CREATE INDEX ix_messages_active_correlation_id_global_position
-  ON beckett.messages_active ((metadata ->> '$correlation_id'::text), global_position)
-  WHERE ((metadata ->> '$correlation_id'::text) IS NOT NULL);
