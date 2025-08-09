de6dfdb8e7ded6e88bc7b3c61336f39078e5eecd
Matt Burton <matt.burton@gmail.com>
2025-08-08 22:36:46 -0400

    recurring messages should be sent with a correlation id (#106)

--- src/Beckett/Scheduling/Services/RecurringMessageService.cs
+++ src/Beckett/Scheduling/Services/RecurringMessageService.cs
@@ -54,8 +54,7 @@ public class RecurringMessageService(
                 foreach (var streamGroup in results.GroupBy(x => x.StreamName))
                 {
                     recurringMessages.AddRange(
-                        streamGroup.Select(
-                            recurringMessage => new ScheduledMessageContext(
+                        streamGroup.Select(recurringMessage => new ScheduledMessageContext(
                                 recurringMessage.StreamName,
                                 recurringMessage.ToMessage()
                             )
@@ -67,10 +66,12 @@ public class RecurringMessageService(
 
                 foreach (var recurringMessagesForStream in recurringMessages.GroupBy(x => x.StreamName))
                 {
+                    var correlationId = Guid.NewGuid().ToString();
+
                     await messageStore.AppendToStream(
                         recurringMessagesForStream.Key,
                         ExpectedVersion.Any,
-                        recurringMessagesForStream.Select(x => x.Message),
+                        recurringMessagesForStream.Select(x => x.Message.WithCorrelationId(correlationId)),
                         stoppingToken
                     );
                 }
@@ -130,7 +131,9 @@ public class RecurringMessageService(
         var nextOccurrence = cronExpression.GetNextOccurrence(DateTimeOffset.UtcNow, timeZone);
 
         command.Parameters.Add(new NpgsqlParameter<string> { Value = recurringMessage.Name });
-        command.Parameters.Add(new NpgsqlParameter<DateTimeOffset> { Value = nextOccurrence?.ToUniversalTime(), IsNullable = true });
+        command.Parameters.Add(
+            new NpgsqlParameter<DateTimeOffset> { Value = nextOccurrence?.ToUniversalTime(), IsNullable = true }
+        );
 
         batch.BatchCommands.Add(command);
     }
